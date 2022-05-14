f2769d1fa6155d005e473a605edec46665cc340e
Sancara <santiagocaraballo@gmail.com>
2022-05-14 12:29:22 -0300

    connecting the views with the urls

--- Car-Selling/cars_site/cars/urls.py
+++ Car-Selling/cars_site/cars/urls.py
@@ -1,5 +1,11 @@
+from http.client import HTTPResponse
 from django.urls import path
+from . import views
+
+app_name = 'cars'
 
 urlpatterns = [
-    
+    path('list/', views.list_view, name='list'),
+    path('add/', views.add_view, name='add'),
+    path('delete/', views.delete_view, name='delete'),
 ]
