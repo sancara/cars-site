23b28a512cbf59cbbe21cc5e82e26927ab45d14a
Sancara <santiagocaraballo@gmail.com>
2022-05-14 12:22:42 -0300

    views.py: connecting with templates

--- Car-Selling/cars_site/cars/views.py
+++ Car-Selling/cars_site/cars/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list_view(request):
+    return render(request, 'cars/list.html')
+
+def add_view(request):
+    return render(request, 'cars/add.html')
+
+def delete_view(request):
+    return render(request, 'cars/delete.html')
