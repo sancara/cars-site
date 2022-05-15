af9de9f37f6da923af1ff07509b8fd0fc309813e
Sancara <santiagocaraballo@gmail.com>
2022-05-15 08:46:08 -0300

    create a basic form for add.html

--- Car-Selling/cars_site/cars/views.py
+++ Car-Selling/cars_site/cars/views.py
@@ -1,11 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def list_view(request):
-    return render(request, 'cars/list.html')
+    all_cars = models.Car.objects.all()
+    context = {'todos_los_autos': all_cars}
+
+    return render(request, 'cars/list.html', context=context)
+
 
 def add_view(request):
     return render(request, 'cars/add.html')
+
 
 def delete_view(request):
     return render(request, 'cars/delete.html')
