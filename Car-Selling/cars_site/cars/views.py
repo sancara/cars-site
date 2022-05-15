215632a0e952524a6166f1d35d4ff7f64614b26f
Sancara <santiagocaraballo@gmail.com>
2022-05-15 10:11:34 -0300

    refactor the style for add & list form, create delete form

--- Car-Selling/cars_site/cars/views.py
+++ Car-Selling/cars_site/cars/views.py
@@ -1,18 +1,34 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from . import models
 
 # Create your views here.
 def list_view(request):
     all_cars = models.Car.objects.all()
-    context = {'todos_los_autos': all_cars}
+    context = {'all_cars': all_cars}
 
     return render(request, 'cars/list.html', context=context)
 
 
 def add_view(request):
-    return render(request, 'cars/add.html')
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        #insert the input to the model
+        models.Car.objects.create(brand=brand, year=year)
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 
 def delete_view(request):
-    return render(request, 'cars/delete.html')
+    if request.POST:
+        pk = int(request.POST['deletion'])
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('pk not found')
+            return redirect(reverse('cars:delete'))
+    else:    
+        return render(request, 'cars/delete.html')
